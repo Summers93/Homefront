474e71a0f4a7f2b2c53d1eb76b6e02d56ada4ce8
Summers93 <11summt@gmail.com>
2018-07-21 01:21:18 -0400

    enter

---
+++ Homefront Login Register/loginregister.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homefront</title>
+        <link rel="stylesheet" href="loginregister.css">
+        <link href="https://fonts.googleapis.com/css?family=Unica+One:regular" rel="stylesheet" />
+    </head>
+    <body>
+        <?php include "loginregister.html" ?>
+    </body>
+</html>
